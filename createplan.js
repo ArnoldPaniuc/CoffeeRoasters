35a8fef2156dfb26e9d5d0547c36a9a263db871b
Arnold <kasi.rock94@gmail.com>
2023-08-14 15:57:13 +0100

    updated with js selection

--- createplan.js
+++ createplan.js
@@ -1,9 +1,26 @@
-let popup = document.getElementById("popup");
+// let popup = document.getElementById("popup");
 
-function openPopup() {
-    popup.classList.add("open-popup");
-}
+// function openPopup() {
+//     popup.classList.add("open-popup");
+// }
 
-function closePopup() {
-    popup.classList.remove("open-popup");
-}
+// function closePopup() {
+//     popup.classList.remove("open-popup");
+// }
+
+
+// code to change background css for "how do you drink your coffee? and the below elements"
+document.addEventListener('DOMContentLoaded', function() {
+    const capSections = document.querySelectorAll('.cap');
+    capSections.forEach(function(capSection) {
+        const capsulesElements = capSection.querySelectorAll('.capsules');
+        capsulesElements.forEach(function(capsulesElement) {
+            capsulesElement.addEventListener('click', function() {
+                capsulesElements.forEach(function(element) {
+                    element.classList.remove('selected');
+                });
+                this.classList.add('selected');
+            });
+        });
+    });
+});
